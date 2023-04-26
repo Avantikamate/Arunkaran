2490132678a2411ca0ca65abaacf7ecd9aa41719
Avantikamate <131361139+Avantikamate@users.noreply.github.com>
2023-04-26 20:40:11 +0530

    Update Gitupdate.java

--- src/main/java/Module_login1/Gitupdate.java
+++ src/main/java/Module_login1/Gitupdate.java
@@ -4,7 +4,8 @@ public class Gitupdate {
 	
 	public static void main(String[] args) {
 		System.out.println("krish change the code");
-		system.out.println"Avantika make changes");
+		system.out.println("Avantika make changes");
+		system.out.println("akshita pagal hai");
 	}
 
 }
