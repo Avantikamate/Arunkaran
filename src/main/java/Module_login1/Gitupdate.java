1f7075f3432f756c429be46f036d325a5cedb842
Avantikamate <131361139+Avantikamate@users.noreply.github.com>
2023-04-26 19:53:35 +0530

    Update Gitupdate.java

--- src/main/java/Module_login1/Gitupdate.java
+++ src/main/java/Module_login1/Gitupdate.java
@@ -3,7 +3,7 @@ package Module_login1;
 public class Gitupdate {
 	
 	public static void main(String[] args) {
-		System.out.println("git updated");
+		System.out.println("krish change the code");
 	}
 
 }
